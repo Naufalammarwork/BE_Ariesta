e07ee38196d61cf0a074ed35b71a8e162a22760e
Naufalammarwork <109495303+Naufalammarwork@users.noreply.github.com>
2022-07-26 16:53:39 +0700

    Try connect to paradox error
    Driver error

--- src/model/dbConnection.js
+++ src/model/dbConnection.js
@@ -1,13 +1,18 @@
-const mysql = require('mysql');
+const odbc = require('odbc');
 
-const db = mysql.createPool({
-  host: '192.168.8.253',
-  user: 'root',
-  password: 'root',
-  database: 'bokormas'
-});
+const path = "D:/Quick access/Documents/tables"
+// const db = odbc.connect("DRIVER={Microsoft Paradox (*.db )};DSN=ariesta;Database=" + path);
 
-db.getConnection(function(err) {
+const connectionConfig = {
+  connectionString: 'DSN=ariesta',
+  connectionTimeout: 10,
+  loginTimeout: 10,
+  driver:"Microsoft Paradox Driver (*.db )"
+}
+
+const db = odbc.connect(connectionConfig);
+
+db.then(function (err) {
   if (err) throw err;
   console.log('Database is connected successfully !');
 });
